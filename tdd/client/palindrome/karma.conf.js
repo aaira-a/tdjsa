a935dc31287d8bd82890bad4a680c907c657456c
azam alias <azamalias@gmail.com>
2016-12-18 09:25:13 +0800

    configured karma with chai and file patterns

--- tdd/client/palindrome/karma.conf.js
+++ tdd/client/palindrome/karma.conf.js
@@ -10,11 +10,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
+        './test/**/*.js',
+        './src/**/*.js'
     ],
 
 
