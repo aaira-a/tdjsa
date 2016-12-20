c87dca373667bf7e3bb101b4da3e4e62b03c596c
azam alias <azamalias@gmail.com>
2016-12-20 23:06:09 +0800

    added signal function to notify mocha of test completion, test now fails as expected

--- async/files/test/files-test.js
+++ async/files/test/files-test.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 var linesCount = require('../src/files');
 
 describe('test server-side callback', function() {
-    it('should return correct lines count for a valid file', function() {
+    it('should return correct lines count for a valid file', function(done) {
         var callback = function(count) {
             expect(count).to.be.eql(-2319);
         };
