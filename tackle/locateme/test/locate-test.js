0237de09de2288e32655586c1400261f6fb9132a
azam alias <azamalias@gmail.com>
2016-12-22 23:59:53 +0800

    modified locate test to use sinon mock

--- tackle/locateme/test/locate-test.js
+++ tackle/locateme/test/locate-test.js
@@ -1,15 +1,9 @@
 describe('locate test', function() {
-    it('should register handlers with getCurrentPosition', function(done) {
-
-        var original = navigator.geolocation.getCurrentPosition;
-
-        navigator.geolocation.getCurrentPosition = function(success, error) {
-            expect(success).to.be.eql(onSuccess);
-            expect(error).to.be.eql(onError);
-            done();
-        };
+    it('should register handlers with getCurrentPosition', function() {
+        var getCurrentPositionMock = sandbox.mock(navigator.geolocation)
+            .expects('getCurrentPosition')
+            .withArgs(onSuccess, onError);
 
         locate();
-        navigator.geolocation.getCurrentPosition = original;
     });
 });
