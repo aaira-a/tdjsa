904a7dd99ea692c7be5000f58bda7bb9d5940b41
azam alias <azamalias@gmail.com>
2016-12-31 16:02:59 +0800

    added canary test

---
+++ testnode/stockfetch/test/stockfetch-test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('Stockfetch tests', function() {
+    it('should pass this canary test', function() {
+        expect(true).to.be.true;
+    });
+});
