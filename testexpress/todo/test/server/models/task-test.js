1f1cd11ef72b528ebbeaa79a94e61cba23d7ef4c
azam alias <azamalias@gmail.com>
2017-01-17 23:39:47 +0800

    added test fixtures and teardown for model test in beforeach and aftereach

--- testexpress/todo/test/server/models/task-test.js
+++ testexpress/todo/test/server/models/task-test.js
@@ -14,4 +14,25 @@ describe('task model tests', function() {
     after(function() {
         db.close();
     });
+
+    var id = function(idValue) {
+        return new ObjectId(idValue);
+    };
+
+    beforeEach(function(done) {
+        sampleTask = {name: 'a new task', month: 12, day: 10, year: 2016};
+
+        sampleTasks = [
+            {_id: id('123412341240'), name: 'task1', month: 10, day: 5, year: 2016},
+            {_id: id('123412341241'), name: 'task2', month: 11, day: 2, year: 2016},
+            {_id: id('123412341242'), name: 'task3', month: 12, day: 8, year: 2016},
+        ];
+
+        db.get().collection('tasks').insert(sampleTasks, done);
+    });
+
+    afterEach(function(done) {
+        db.get().collection('tasks').drop(done);
+    });
+
 });
