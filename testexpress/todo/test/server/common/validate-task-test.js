4770c06c27e222637b7ade08aea31ff043eafc06
azam alias <azamalias@gmail.com>
2017-01-19 23:53:38 +0800

    added initial test for task validator function, which currently returns true

---
+++ testexpress/todo/test/server/common/validate-task-test.js
@@ -0,0 +1,20 @@
+var expect = require('chai').expect;
+var validateTask = 
+    require('../../../public/javascripts/common/validate-task');
+
+describe('validate task tests', function() {
+    var sampleTask;
+
+    var expectFailForProperty = function(property, value) {
+        sampleTask[property] = value;
+        expect(validateTask(sampleTask)).to.be.false;
+    };
+
+    beforeEach(function() {
+        sampleTask = {name: 'a new task', month: 12, day: 10, year: 2016};
+    });
+
+    it('should return true for valid task', function() {
+        expect(validateTask(sampleTask)).to.be.true;
+    });
+});
