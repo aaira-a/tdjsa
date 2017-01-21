7beda19c1c8728f9c3af23be62bf9470314aeb42
azam alias <azamalias@gmail.com>
2017-01-22 00:25:19 +0800

    added get / handler, tested using model & response stubs

--- testexpress/todo/routes/tasks.js
+++ testexpress/todo/routes/tasks.js
@@ -2,6 +2,10 @@ var express = require('express');
 var task = require('../models/task');
 var router = express.Router();
 
-router.get('/', undefined);
+router.get('/', function(req, res, next) {
+    task.all(function(err, tasks) {
+        res.send(tasks);
+    });
+});
 
 module.exports = router;
--- testexpress/todo/test/server/routes/tasks-test.js
+++ testexpress/todo/test/server/routes/tasks-test.js
@@ -26,4 +26,27 @@ describe('tasks route test', function() {
     it('should register URI / for get', function() {
         expect(router.get.calledWith('/', sandbox.match.any)).to.be.true;
     });
+
+    var stubResSend = function(expected, done) {
+        return { send: function(data) {
+            expect(data).to.be.eql(expected);
+            done();
+        }};
+    };
+
+    it("get / handler should call model's all & return result",
+        function(done) {
+        var sampleTasks = [{name: 't1', month: 12, day: 1, year: 2016}];
+
+        sandbox.stub(task, 'all', function(callback) {
+            callback(null, sampleTasks);
+        });
+
+        var req = {};
+        var res = stubResSend(sampleTasks, done);
+
+        var registeredCallback = router.get.firstCall.args[1];
+        registeredCallback(req, res);
+    });
+
 });
