86198f8f2d9f9af9b249df24d719c381ae1f1bd5
azam alias <azamalias@gmail.com>
2017-05-04 23:05:25 +0800

    added remaining tests for jquery front-end

--- testendtoend/todo/test/integration/tasksj-test.js
+++ testendtoend/todo/test/integration/tasksj-test.js
@@ -14,4 +14,25 @@ describe('tasks ui test', function() {
     it('should show correct task count', function() {
         expect(element(by.id('taskscount')).getText()).to.be.eventually.eql('4');
     });
+
+    it('should display tasks on the page', function() {
+        expect(element(by.id('tasks')).getText()).to.eventually.contain('Test Models');
+        expect(element(by.id('tasks')).getText()).to.eventually.contain('Test UI');
+    });
+
+    it('should successfully add a task', function() {
+        element(by.id('name')).sendKeys('Create Quality Code');
+        element(by.id('date')).sendKeys('12/15/2016');
+        element(by.id('submit')).click();
+
+        expect(element(by.id('message')).getText()).to.eventually.contain('task added');
+        expect(element(by.id('tasks')).getText()).to.eventually.contain('Create Quality Code');
+    });
+
+    it('should successfully delete a task', function() {
+        element.all(by.linkText('delete')).get(1).click();
+
+        expect(element(by.id('message')).getText()).to.eventually.contain('task deleted');
+        expect(element(by.id('tasks')).getText()).to.eventually.not.contain('Test Routes');
+    });
 });
